cf264a9363d4d11783aa3c2273f7ef414b3235ab
Gabriel Victor <gabriel.victor@liferay.com>
2022-05-19 15:39:48 -0300

    LPS-151043 removed delete event for delete key not to delete assignment

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/SidebarHeader.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/SidebarHeader.js
@@ -61,7 +61,7 @@ export default function SidebarHeader({
 
 	const handleKeyDown = (event) => {
 		if (
-			(event.key === 'Backspace') &&
+			event.key === 'Backspace' &&
 			document.querySelectorAll('.form-control:focus').length === 0 &&
 			document.querySelectorAll('.CodeMirror-focused').length === 0
 		) {
