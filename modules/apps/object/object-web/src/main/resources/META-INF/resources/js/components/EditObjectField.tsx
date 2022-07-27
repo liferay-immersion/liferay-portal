ec9d91ea67f28ad34bd04b0843180fdb3706dfa0
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2022-07-27 15:43:43 -0300

    LPS-159026 Added prop to the InputLocalized component to disable the localizable button

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/EditObjectField.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/EditObjectField.tsx
@@ -530,6 +530,7 @@ export default function EditObjectField({
 		>
 			<Card title={Liferay.Language.get('basic-info')}>
 				<InputLocalized
+					disableFlag={values.system && objectName !== 'AccountEntry'}
 					disabled={
 						values.system && objectName !== 'AccountEntry'
 							? disabled
