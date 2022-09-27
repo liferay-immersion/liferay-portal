c49953f9aa2577f557fea52d81869b7cc74226b6
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2022-09-27 16:07:53 -0300

    LPS-162822 Create method to validade next Object State

--- modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectEntryValuesException.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectEntryValuesException.java
@@ -192,6 +192,34 @@ public class ObjectEntryValuesException extends PortalException {
 
 	}
 
+	public static class invalidObjectStateChange
+		extends ObjectEntryValuesException {
+
+		public invalidObjectStateChange(
+			String sourceObjectState, String targetObjectState) {
+
+			super(
+				String.format(
+					"Object state \"%s\" cannot be changed to \"%s\"",
+					sourceObjectState, targetObjectState));
+
+			_sourceObjectState = sourceObjectState;
+			_targetObjectState = targetObjectState;
+		}
+
+		public String getSourceObjectState() {
+			return _sourceObjectState;
+		}
+
+		public String getTargetObjectState() {
+			return _targetObjectState;
+		}
+
+		private final String _sourceObjectState;
+		private final String _targetObjectState;
+
+	}
+
 	public static class ListTypeEntry extends ObjectEntryValuesException {
 
 		public ListTypeEntry(String objectFieldName) {
